b2b2c8ccccb90f359ccbd2c9083da823918a4235
test <nivaan@vl965-172-31-229-181.wireless.umass.edu>
2026-03-09 17:40:26 -0400

    Add file database functions

--- backend/db.py
+++ backend/db.py
@@ -10,3 +10,32 @@ if not db_url or not db_secret:
     raise ValueError("Missing env vars")
 
 db_client = create_client(db_url, db_secret)
+
+# -------- FILE DATABASE FUNCTIONS -------- #
+
+def create_file(supabase, workspace_id, title, storage_path):
+    response = supabase.table("files").insert({
+        "workspace_id": workspace_id,
+        "title": title,
+        "storage_path": storage_path
+    }).execute()
+
+    return response
+
+
+def get_files_by_workspace(supabase, workspace_id):
+    response = supabase.table("files") \
+        .select("*") \
+        .eq("workspace_id", workspace_id) \
+        .execute()
+
+    return response
+
+
+def delete_file(supabase, file_id):
+    response = supabase.table("files") \
+        .delete() \
+        .eq("id", file_id) \
+        .execute()
+
+    return response
